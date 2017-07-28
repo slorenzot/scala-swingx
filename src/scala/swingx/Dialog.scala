0ec518bbfc47c87117100b4e1cb51bad61d9c680
Soulberto Lorenzo <slorenzot@gmail.com>
2017-07-27 21:50:52 -0400

    some changes

--- src/scala/swingx/Dialog.scala
+++ src/scala/swingx/Dialog.scala
@@ -1,35 +1,71 @@
 package scala.swingx
 
 import java.awt.Component
-import javax.swing.ImageIcon
+import java.awt.event._
+import javax.swing.{ImageIcon, JComponent, KeyStroke, WindowConstants}
 
 /**
   * Created by Soulberto on 7/27/2017.
   */
 case class Dialog[T](val view: javax.swing.JDialog) extends Window {
 
-//  var parent: Window = view
+  var okEvent = () => println("OK")
+  var cancelEvent = () => println("Cancelled by user")
 
   def title(title: String): Dialog[T] = {
     view.setTitle(title)
     this
   }
 
+  /** Asigna un icono a la ventana de dialogo
+    *
+    * @param icon imagen en formato ImageIcon
+    * @return
+    */
   def icon(icon: ImageIcon): Dialog[T] = {
     view.setIconImage(icon.getImage)
     this
   }
 
+  /**
+    * Centra la ventana de dialogo al componente indicado, si no
+    * se indica ningun componente centra en pantalla
+    *
+    * @param parent
+    * @return
+    */
   def center(parent: Component = null): Dialog[T] = {
     view.setLocationRelativeTo(parent)
     this
   }
 
+  /** Muestra la ventana de dialogo. Realiza algunas acciones adicionales
+    * como; setea la apariencia de swing a la usada por el Sistema Operativo,
+    * agrega comportamiento ESCAPE de teclado para cancelar la ventana, por
+    * defecto centra el dialogo en la ventana que lo invoco, trae el dialogo
+    * al frente y le da el foco.
+    */
   def display: Unit = {
     this.defaultLAF(view)
 
+    view.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
+    view.addWindowListener(new WindowAdapter() {
+      override def windowClosing(event: WindowEvent): Unit = {
+        dispose()
+        cancelEvent()
+      }
+    })
+
+    view.getRootPane.registerKeyboardAction(new ActionListener() {
+      override def actionPerformed(event: ActionEvent): Unit = {
+        dispose()
+        cancelEvent()
+      }
+    }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
     view.pack
     view.setLocationRelativeTo(null)
+    view.setModal(true)
     view.setVisible(true)
     view.toFront
     view.requestFocusInWindow
--- src/scala/swingx/Main.scala
+++ src/scala/swingx/Main.scala
@@ -7,12 +7,12 @@ object Main extends App {
 
   Frame.of(new swingexamples.Frame)
     .icon(Image.file("/resources/icons/default.png").toIcon)
-    .title("ventana de prueba")
+    .title("Ventana de prueba")
     .maximize
     .display
 
   Dialog.of(new swingexamples.Dialog(null, true))
-//      .from(window)
+    //      .from(window)
     .icon(Image.file("/resources/icons/default.png").toIcon)
     .title("Dialog de prueba")
     .center()
