89c732c0d09dd5e0ff921784f615c574d350548e
Soulberto Lorenzo <slorenzot@gmail.com>
2017-08-08 07:04:56 -0400

    some changes

--- src/scala/swingx/binding/TableBinding.scala
+++ src/scala/swingx/binding/TableBinding.scala
@@ -2,6 +2,7 @@ package scala.swingx.binding
 
 import java.awt.event.{KeyEvent, KeyListener}
 import javax.swing.event.{ListSelectionEvent, ListSelectionListener, TableModelEvent, TableModelListener}
+import javax.swing.table.DefaultTableModel
 
 /**
   * Created by Soulberto Lorenzo on 8/5/2017.
@@ -9,14 +10,34 @@ import javax.swing.event.{ListSelectionEvent, ListSelectionListener, TableModelE
 case class TableBinding(swingComponent: javax.swing.JTable) {
 
   private def source: javax.swing.JTable = swingComponent
+  private var model: DefaultTableModel = swingComponent.getModel.asInstanceOf[DefaultTableModel]
 
-  def change(action: () => Unit): TableBinding = {
+  def values(): Array[AnyRef] = {
+    var rows = Array.empty
+
+    for {
+      row <- swingComponent.getModel.getRowCount
+      col <- swingComponent.getModel.getColumnCount
+
+      rows :+ swingComponent.getModel.getValueAt(row, col)
+    } yield
+  }
+
+  def populate(supplier: () => Array[AnyRef]): TableBinding = {
+    for (row <- supplier()) {
+      model.addRow()
+    }
+    this
+  }
+
+  def cellChange(action: () => Unit): TableBinding = ???
+
+  def rowChange(action: () => Unit): TableBinding = {
     source.getModel.addTableModelListener(new TableModelListener() {
       override def tableChanged(e: TableModelEvent) = {
 
       }
     })
-
     this
   }
 
@@ -24,7 +45,6 @@ case class TableBinding(swingComponent: javax.swing.JTable) {
     source.getSelectionModel.addListSelectionListener(new ListSelectionListener() {
       override def valueChanged(e: ListSelectionEvent) = {}
     })
-
     this
   }
 
@@ -32,7 +52,6 @@ case class TableBinding(swingComponent: javax.swing.JTable) {
     source.getSelectionModel.addListSelectionListener(new ListSelectionListener() {
       override def valueChanged(e: ListSelectionEvent) = {}
     })
-    
     this
   }
 
